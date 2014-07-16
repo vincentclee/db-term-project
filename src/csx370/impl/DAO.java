6cf0e5d71d8189cd3c8d97912ac3bea42e298e4c
C.J. Bennett <cecil@uga.edu>
2014-07-15 23:38:41 -0400

    added connector table creators and changed the three main creator functions to return objects

--- src/csx370/impl/DAO.java
+++ src/csx370/impl/DAO.java
@@ -57,48 +57,70 @@ public class DAO
   }// ctor
 
   /**
-   * Create a new user account in the DB with the given details.
+   * Create a new user account in the DB with the given details and return a User
+   * object detailing the newly created user
    *
    * @param username the unique username of the user
    * @param email the unique registration email of the user
    * @param displayName the non-unique display name of the user
    * @param specialization the user's specialization
    * @param password the user's password
+   * @return a user object containing all info about this user from the User table
    */
-  public void createUser(String username, String email, String displayName, String specialization, String password)
+  public User createUser(String username, String email, String displayName, String specialization, String password)
   {
     ////////////////////////////////////////////////////////////////////////////////////////////////////
     // TODO
     // check prepared statement for schema errors when they're made
     ////////////////////////////////////////////////////////////////////////////////////////////////////
+    
+    // user object to return
+    User user =  null;
 
     try
     {
-      PreparedStatement insertUser = this.conn.prepareStatement("INSERT INTO User(Username, Email, DisplayName, Specialization, Password) VALUES (?,?,?,?,?)");
+      // insert info into db
+      PreparedStatement insertUser = this.conn.prepareStatement("INSERT INTO User(UserName, Email, DisplayName, Specialization, Password) VALUES (?,?,?,?,UNHEX(SHA1(?)))");
       insertUser.setString(1, username);
       insertUser.setString(2, email);
       insertUser.setString(3, displayName);
       insertUser.setString(4, specialization);
-      insertUser.setString(5, this.encryptString(password));
+      insertUser.setString(5, password);
       
       insertUser.executeUpdate();
+
+      // get userID from freshly inserted row
+      int userID;
+      PreparedStatement selectUser = this.conn.prepareStatement("SELECT UserID FROM User WHERE UserName = (?)");
+      selectUser.setString(1, username);
+
+      ResultSet rs = selectUser.executeQuery();
+      rs.next();
+      userID = rs.getInt("UserID");
+      
+      // create user object to return
+      user = new User(userID, username, email, displayName, specialization);
     }// try
     catch(Exception e)
     {
       System.err.println("Error creating user account: " + e.getMessage());
     }// catch
+
+    return user;
   }// createUser
 
   /**
-   * Create a new project in the DB with the given details.
+   * Create a new project in the DB with the given details and return a Project 
+   * object detailing the newly created project
    *
    * @param title
    * @param description
    * @param targetDate
    * @param managerID
    * @param status
+   * @return a Project object containing all info about this project from the Project table
    */
-  public void createProject(String title, String description, String targetDate, int managerID, String status)
+  public Project createProject(String title, String description, String targetDate, int managerID, String status)
   {
     ////////////////////////////////////////////////////////////////////////////////////////////////////
     // TODO
@@ -107,9 +129,13 @@ public class DAO
     // maybe use default value for status?
     // update parameter comments
     ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    // project object to return
+    Project project = null;
     
     try
     {
+      // insert info into db
       PreparedStatement insertProject = this.conn.prepareStatement("INSERT INTO Project(Title, Description, TargetDate, Manager, Status) VALUES (?,?,?,?,?)");
       insertProject.setString(1, title);
       insertProject.setString(2, description);
@@ -118,15 +144,34 @@ public class DAO
       insertProject.setString(5, status);
       
       insertProject.executeUpdate();
+
+      // get projectID from freshly inserted row
+      int projectID;
+      PreparedStatement selectProject = this.conn.prepareStatement("SELECT ProjectID FROM Project WHERE Title = (?) AND Description = (?) AND TargetDate = (?) AND Manager = (?) AND Status = (?)");
+      selectProject.setString(1, title);
+      selectProject.setString(2, description);
+      selectProject.setString(3, targetDate);
+      selectProject.setInt(4, managerID);
+      selectProject.setString(5, status);
+
+      ResultSet rs = selectProject.executeQuery();
+      rs.next();
+      projectID = rs.getInt("ProjectID");
+      
+      // create prject object to return
+      project = new Project(projectID, title, description, targetDate, managerID, status);
     }// try
     catch(Exception e)
     {
       System.err.println("Error creating project: " + e.getMessage());
     }// catch
+    
+    return project;
   }// createProject
 
   /**
-   * Create a new task in the DB with the given details.
+   * Create a new task in the DB with the given details and return a Task object
+   * detailing the newly created task
    *
    * @param type
    * @param priority
@@ -138,8 +183,9 @@ public class DAO
    * @param description
    * @param scope
    * @param status
+   * @return a Task object containing all info about this task from the Task table
    */
-  public void createTask(String type, String priority, int projectID, boolean hasDependency, 
+  public Task createTask(String type, String priority, int projectID, boolean hasDependency, 
 			 String deadline, String title, String notes, String description, 
 			 String scope, String status)
   {
@@ -150,6 +196,9 @@ public class DAO
     // maybe use default value for status?
     // update parameter comments
     ////////////////////////////////////////////////////////////////////////////////////////////////////
+    
+    // task object to return
+    Task task = null;
     
     try
     {
@@ -166,15 +215,38 @@ public class DAO
       insertTask.setString(10, status);
       
       insertTask.executeUpdate();
+
+      // get taskID from freshly inserted row
+      int taskID;
+      PreparedStatement selectTask = this.conn.prepareStatement("SELECT TaskID FROM Task WHERE Type = (?) AND Priority = (?) AND ProjectID = (?) AND HasDependency = (?) AND Deadline = (?) AND Title = (?) AND Notes = (?) AND Description = (?) AND Scope = (?) AND Status = (?)");
+      selectTask.setString(1, type);
+      selectTask.setString(2, priority);
+      selectTask.setInt(3, projectID);
+      selectTask.setBoolean(4, hasDependency);
+      selectTask.setString(5, deadline);
+      selectTask.setString(6, title);
+      selectTask.setString(7, notes);
+      selectTask.setString(8, description);
+      selectTask.setString(9, scope);
+      selectTask.setString(10, status);
+
+      ResultSet rs = selectTask.executeQuery();
+      rs.next();
+      taskID = rs.getInt("TaskID");
+
+      // create prject object to return
+      task = new Task(taskID, projectID, hasDependency, type, priority, deadline, title, notes, description, scope, status);
     }// try
     catch(Exception e)
     {
       System.err.println("Error creating task: " + e.getMessage());
     }// catch
+
+    return task;
   }// createTask
 
   /**
-   * Connect a specific user to a specific task indentified by their respective IDs.
+   * Connect a user and a task, indentified by their respective IDs.
    *
    * @param taskID the taskID to connect to the given userID
    * @param userID the userID to connect to the given taskID
@@ -196,9 +268,68 @@ public class DAO
     }// try
     catch(Exception e)
     {
-      System.err.println("Error creating task: " + e.getMessage());
+      System.err.println("Error creating usertask: " + e.getMessage());
     }// catch
   }// createUserTask
+
+  /**
+   * Connect a task and a project, indentified by their respective IDs.
+   *
+   * @param projectID the projectID to connect to the given taskID
+   * @param taskID the taskID to connect to the given projectID
+   */
+  public void createProjectTask(int projectID, int taskID)
+  {
+    /////////////////////////////////////////////////////////////////////////////////////////////////////
+    // TODO
+    // check prepared statement for schema errors when they're made
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    
+    try
+    {
+      PreparedStatement insertProjectTask = this.conn.prepareStatement("INSERT INTO ProjectTask(ProjectID, TaskID) VALUES (?,?)");
+      insertProjectTask.setInt(1, projectID);
+      insertProjectTask.setInt(2, taskID);
+
+      insertProjectTask.executeUpdate();
+    }// try
+    catch(Exception e)
+    {
+      System.err.println("Error creating projecttask: " + e.getMessage());
+    }// catch
+  }// createProjectTask
+
+  /**
+   * Connect user and a project, indentified by their respective IDs.
+   *
+   * @param userID the userID to connect to the given projectID
+   * @param projectID the projectID to connect to the given taskID
+   * @param commits
+   * @param contributions
+   */
+  public void createProjectUser(int userID, int projectID, String commits, String contributions)
+  {
+    /////////////////////////////////////////////////////////////////////////////////////////////////////
+    // TODO
+    // check prepared statement for schema errors when they're made
+    // update parameter comments
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    
+    try
+    {
+      PreparedStatement insertProjectUser = this.conn.prepareStatement("INSERT INTO ProjectUser(ProjectID, TaskID, Commits, Contributions) VALUES (?,?,?,?)");
+      insertProjectUser.setInt(1, projectID);
+      insertProjectUser.setInt(2, userID);
+      insertProjectUser.setString(3, commits);
+      insertProjectUser.setString(4, contributions);
+
+      insertProjectUser.executeUpdate();
+    }// try
+    catch(Exception e)
+    {
+      System.err.println("Error creating projectuser: " + e.getMessage());
+    }// catch
+  }// createProjectUser
 
   /**
    * Check the connection to the db.
@@ -248,16 +379,13 @@ public class DAO
       this.conn.prepareStatement("TRUNCATE TABLE User").executeUpdate();
       this.conn.prepareStatement("TRUNCATE TABLE Project").executeUpdate();
       this.conn.prepareStatement("TRUNCATE TABLE Task").executeUpdate();
+      this.conn.prepareStatement("TRUNCATE TABLE ProjectTask").executeUpdate();
+      this.conn.prepareStatement("TRUNCATE TABLE UserTask").executeUpdate();
+      this.conn.prepareStatement("TRUNCATE TABLE ProjectUser").executeUpdate();
     }// try
     catch(Exception e)
     {
       System.err.println("Error resetting database: " + e.getMessage());
     }// catch
   }// resetDB
-
-  // hashes the given string
-  private String encryptString(String s)
-  {
-    return (new Integer(s.hashCode())).toString();
-  }// encryptString
 }// DAO
