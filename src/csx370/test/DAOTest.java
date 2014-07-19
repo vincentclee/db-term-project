56d74af5bf9c1392208f25b254b974597f0254af
C.J. Bennett <cecil@uga.edu>
2014-07-19 18:49:22 -0400

    really wish i could just compile this stuff on nike

--- src/csx370/test/DAOTest.java
+++ src/csx370/test/DAOTest.java
@@ -119,10 +119,10 @@ public class DAOTest extends TestCase
     assertEquals("9", empty, dao.getUserTasksForProjectByID(user4.getUserID(), proj3.getProjectID()));
     
     // by cookieid
-    assertEquals("10", ala, dao.getUserTasksForProjectByCookieID(user1.getUserID(), proj1.getProjectID()));
-    assertEquals("11", calb, dao.getUserTasksForProjectByCookieID(user3.getUserID(), proj2.getProjectID()));
-    assertEquals("12", elvisc, dao.getUserTasksForProjectByCookieID(user5.getUserID(), proj3.getProjectID()));
-    assertEquals("13", empty, dao.getUserTasksForProjectByCookieID(user4.getUserID(), proj3.getProjectID()));
+    assertEquals("10", ala, dao.getUserTasksForProjectByCookieID(user1.getCookieID(), proj1.getProjectID()));
+    assertEquals("11", calb, dao.getUserTasksForProjectByCookieID(user3.getCookieID(), proj2.getProjectID()));
+    assertEquals("12", elvisc, dao.getUserTasksForProjectByCookieID(user5.getCookieID(), proj3.getProjectID()));
+    assertEquals("13", empty, dao.getUserTasksForProjectByCookieID(user4.getCookieID(), proj3.getProjectID()));
   }// testGetUserTasksForProjectFunctions
   
   public void testUserUpdates()
